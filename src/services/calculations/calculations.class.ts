898268160eb8c53172343a8a47ec4c15a17effb9
João Pedro Theodoro Gaino <joao.gaino@eurekalabs.com.br>
2021-03-15 22:57:59 -0300

    fixed bugs

--- src/services/calculations/calculations.class.ts
+++ src/services/calculations/calculations.class.ts
@@ -5,8 +5,6 @@ import {
   Params,
   ServiceMethods,
 } from "@feathersjs/feathers";
-import { Transaction } from "sequelize/types";
-import { toFloat } from "validator";
 import { Application } from "../../declarations";
 import { FinanceTransactionModel } from "../../models/finance-transaction.model";
 import { FinanceTransaction } from "../finance-transaction/finance-transaction.class";
@@ -31,50 +29,8 @@ export class Calculations implements ServiceMethods<Data> {
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
   async find(params?: Params): Promise<Data[] | Paginated<Data> | any> {
     const query: any = params?.query || {};
-    if (query.type === "totalIncome") {
-      const allTransactions = (await this.transactionsService.find()) as Paginated<FinanceTransactionModel>;
-      const valuesIncome = allTransactions.data.map((data) =>
-        data.typeId === 1 ? +data.value : 0
-      );
-      for (const value of valuesIncome) {
-        totalIncome += value;
-      }
-      return {
-        totalIncome: totalIncome,
-      };
-    } else if (query.type === "totalOutcome") {
-      const allTransactions = (await this.transactionsService.find()) as Paginated<FinanceTransactionModel>;
-      const valuesOutcome = allTransactions.data.map((data) =>
-        data.typeId === 2 ? +data.value : 0
-      );
-      for (const value of valuesOutcome) {
-        totalOutcome += value;
-      }
-      return {
-        totalOutcome: totalOutcome,
-      };
-    } else if (query.type === "netBalance") {
-      const allTransactions = (await this.transactionsService.find()) as Paginated<FinanceTransactionModel>;
-      const valuesIncome = allTransactions.data.map((data) =>
-        data.typeId === 1 ? +data.value : 0
-      );
-      const valuesOutcome = allTransactions.data.map((data) =>
-        data.typeId === 2 ? +data.value : 0
-      );
-      for (const value of valuesOutcome) {
-        totalOutcome += value;
-      }
-      for (const value of valuesIncome) {
-        totalIncome += value;
-      }
-      console.log(totalIncome);
-      console.log(totalOutcome);
-      const netBalance = totalIncome - totalOutcome;
-
-      return {
-        netBalance: netBalance,
-      };
-    }
+    const allTransactions = await this.transactionsService.calculateNet();
+    return allTransactions;
   }
 
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
--- src/services/finance-transaction/finance-transaction.class.ts
+++ src/services/finance-transaction/finance-transaction.class.ts
@@ -1,9 +1,37 @@
+import { Paginated } from "@feathersjs/feathers";
 import { Service, SequelizeServiceOptions } from "feathers-sequelize";
 import { Application } from "../../declarations";
+import { FinanceTransactionModel } from "../../models/finance-transaction.model";
 
 export class FinanceTransaction extends Service {
+  app: Application;
   //eslint-disable-next-line @typescript-eslint/no-unused-vars
   constructor(options: Partial<SequelizeServiceOptions>, app: Application) {
     super(options);
+    this.app = app;
+  }
+  async calculateNet(): Promise<any> {
+    const allTransactions = (await this.find()) as Paginated<FinanceTransactionModel>;
+    /* const valuesIncome = allTransactions.data.map((data) =>
+      data.typeId === 1 ? +data.value : 0
+    );
+    const valuesOutcome = allTransactions.data.map((data) =>
+      data.typeId === 2 ? +data.value : 0
+    ); */
+    let valuesIncome = 0,
+      valuesOutcome = 0;
+    for (let i = 0; i < allTransactions.total; i++) {
+      if (allTransactions.data[i].typeId === 1) {
+        valuesIncome += +allTransactions.data[i].value;
+      } else {
+        valuesOutcome += +allTransactions.data[i].value;
+      }
+    }
+    const netIncome = valuesIncome - valuesOutcome;
+    return {
+      valuesIncome,
+      valuesOutcome,
+      netIncome,
+    };
   }
 }
